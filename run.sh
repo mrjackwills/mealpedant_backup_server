894816cac82395c5b8157038a686b670b31f393f
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-10-19 21:40:45 +0000

    chore: run.sh & create_release.sh updated

--- run.sh
+++ run.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# run.sh v0.2.1
+# run.sh v0.2.0
+# 2024-10-19
 
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
+
+# $1 string - question to ask
+# Ask a yes no question, only accepts `y` or `n` as a valid answer, returns 0 for yes, 1 for no
+ask_yn() {
+	while true; do
+		printf "\n%b%s? [y/N]:%b " "${GREEN}" "$1" "${RESET}"
+		read -r answer
+		if [[ "$answer" == "y" ]]; then
+			return 0
+		elif [[ "$answer" == "n" ]]; then
+			return 1
+		else
+			echo -e "${RED}\nPlease enter 'y' or 'n'${RESET}"
+		fi
+	done
+}
 
 error_close() {
 	echo -e "\n${RED}ERROR - EXITED: ${YELLOW}$1${RESET}\n"
@@ -43,8 +60,7 @@ pull_branch() {
 		printf "%s\n" "${GIT_CLEAN}"
 	fi
 	if [[ -n "$GIT_CLEAN" ]]; then
-		ask_yn "Happy to clear git state"
-		if [[ "$(user_input)" =~ ^n$ ]]; then
+		if ! ask_yn "Happy to clear git state"; then
 			exit
 		fi
 	fi
