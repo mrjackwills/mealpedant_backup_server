47dfafd900955d8f9c06d6c1abe80bd0bed920a8
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-02-25 09:50:40 +0000

    feat: Rust edition 2024

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -4,12 +4,12 @@ mod ws_sender;
 
 use futures_util::lock::Mutex;
 use futures_util::{
-    stream::{SplitSink, SplitStream},
     StreamExt, TryStreamExt,
+    stream::{SplitSink, SplitStream},
 };
 use std::sync::Arc;
 use tokio::{net::TcpStream, task::JoinHandle};
-use tokio_tungstenite::{self, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+use tokio_tungstenite::{self, MaybeTlsStream, WebSocketStream, tungstenite::Message};
 
 use connect::ws_upgrade;
 use connection_details::ConnectionDetails;
