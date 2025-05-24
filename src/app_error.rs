dd2973aa132045e761812fb2bc93c0ee64bfd302
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-05-24 05:44:53 +0000

    chore: Rust 1.87.0 linting

--- src/app_error.rs
+++ src/app_error.rs
@@ -10,8 +10,8 @@ pub enum AppError {
     MissingEnv(String),
     #[error("Reqwest Error")]
     Reqwest(#[from] reqwest::Error),
-    #[error("WS Connect")]
-    TungsteniteConnect(#[from] tokio_tungstenite::tungstenite::Error),
+    #[error("WS Connect: '{0}'")]
+    TungsteniteConnect(String),
     #[error("Invalid WS Status Code")]
     WsStatus,
 }
